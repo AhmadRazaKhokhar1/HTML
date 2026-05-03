5eb0ecb9e5a02edfb712ce2c88bea16812764770
Ahmad Raza Khokhar <ark@Ahmads-MacBook-Air.local>
2026-05-03 17:36:01 +0500

    Add js sample

---
+++ HTML/chapter-2/index.js
@@ -0,0 +1,39 @@
+
+// Primitive Data Types in JS
+const string = "Ahmad";
+const number = 9999999999999;
+const bigInt = BigInt(10000000000000000000);
+const bool = true // false
+const name = undefined
+const null_name = null
+const symbol = Symbol()
+
+// Non Primitve 
+const array = [1,3,4,4, "sdas"] // ["a", "b", "c"]
+const data = [
+    {
+    name:"ahmad",
+    class:"12th",
+    age:12
+},
+{
+    name:"ahmad",
+    class:"12th",
+    age:12
+},
+{
+    name:"ahmad",
+    class:"12th",
+    age:12
+}
+]
+const obj = {
+    name:"ahmad",
+    class:"12th",
+    age:12
+}
+
+function abc (){}
+
+const date = new Date();
+const regex = '^[a-zA-Z0-9_-]+\.(jpg|png|gif)$'
