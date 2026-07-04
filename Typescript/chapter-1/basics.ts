68288d3d8948ad354d6a4d8ef85f0d0d92c392a1
Ahmad Raza Khokhar <ark@Ahmads-MacBook-Air.local>
2026-07-04 16:26:35 +0500

    add basic ts

---
+++ Typescript/chapter-1/basics.ts
@@ -0,0 +1,13 @@
+const studentName:string = "Ahmad";
+const age:number = 12;
+const date:Date = new Date();
+const functionAbc = (name:string, age:number, isAdult:boolean) => {
+    return `Name:${name}\n Age:${age}\n ${isAdult?"Is An Adult":"Is Under Aged"}`
+}
+const canMakeJuice:boolean = true
+console.log(studentName)
+
+
+const outputOfFunctinoAbc = functionAbc("Ahmad", 23, true);
+
+console.log(outputOfFunctinoAbc);
